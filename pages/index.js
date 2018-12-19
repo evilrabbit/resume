9023ecf117aa842f3f4a309e736a94435aab1afc
Evil Rabbit <hello@evilrabb.it>
2018-12-19 02:59:57 -0300

    First draft

---
+++ pages/index.js
@@ -0,0 +1,327 @@
+import Logotype from '../components/logotype'
+
+export default () => (
+  <div className="wrapper">
+    <div className="sidebar">
+      <div className="logotype">
+        <Logotype/>
+      </div>
+      <div className="navigation">
+        <a href="#experience">Experience</a>
+        <a href="#oss">OSS Contributions</a>
+        <a href="#press">Press</a>
+        <a href="#education">Education</a>
+        <a href="#info">Information</a>
+      </div>
+    </div>
+    <div className="content">
+      <div id="experience">
+        <div className="work">
+          <h3>Jun 2016-Present</h3>
+          <h2>Head of Design at ZEIT</h2>
+          <p>
+            As the design team lead, I am responsible for curating the visual
+            experience of our brand and products. Being the first designer of
+            the company, I was enabled to develop the design system that we
+            are currently using. We have a growing team of designers and
+            developers which I am now guiding daily in the creative process.
+            <br/>
+            <br/>
+            I’m responsible for:
+            <ul>
+              <li>Brand Identity System</li>
+              <li>Wireframing</li>
+              <li>Release Videos / Announcements</li>
+              <li>Continually scaling the graphic system of <a href="https://zeit.co/" target="_blank">ZEIT</a>, <a href="https://nextjs.org/" target="_blank">Next.js</a> and <a href="https://hyper.is/" target="_blank">Hyper</a> — among others</li>
+              <li>Improving the user experience and the graphic interface of the platform</li>
+              <li>Maintaining a positive culture within the design team</li>
+              <li>Recruiting and hiring for the design team</li>
+            </ul>
+          </p>
+        </div>
+        <div className="work">
+          <h3>Feb 2015 - Apr 2016</h3>
+          <h2>UI Designer at Auth0</h2>
+          <p>
+          My first task was to design <a href="https://sharelock.io/" target="_blank">Sharelock.io</a> (Web / Android App that allows you to share data easily and securely). After releasing the app and its website,
+          I had the opportunity to re-design many important front-facing pages
+          for the company (About, Why Auth0?, Jobs and marketing pages <a href="https://auth0.com/passwordless" target="_blank">Passwordless</a>, AWS Reinvent and <a href="https://auth0.com/guardian" target="_blank">Guardian</a>, to name a few).
+          <br/>
+          <br/>
+          My final responsibility at Auth0 was to lead the project <b>Guardian</b> as a Product Designer, giving me full creative control over the
+          experience, from the blueprints to the final polished app.
+          <br/>
+          <br/>
+          I was responsible for:
+          <ul>
+            <li>Creating Blueprints</li>
+            <li>User Experience Design</li>
+            <li>User Interface Design</li>
+            <li>Product Design</li>
+            <li>Marketing Design</li>
+          </ul>
+          </p>
+        </div>
+        <div className="work">
+          <h3>Dec 2014 - Feb 2015</h3>
+          <h2>Freelance</h2>
+        </div>
+        <div className="work">
+          <h3>Jun 2014 - Dec 2014</h3>
+          <h2>UX/UI Designer at Aerolab</h2>
+          <p>After 4 years designing for an entertainment company I decided to switch back to an agency. I started working at <a href="https://aerolab.co/" target="_blank">Aerolab</a> as a Product Designer.
+          I took care of the following projects: <a href="https://www.lifelock.com/" target="_blank">LifeLock</a>, <a href="https://xapo.com/" target="_blank">Xapo</a> and <a href="https://dribbble.com/shots/1708940-Android-App" target="_blank">Grabbit</a>.</p>
+        </div>
+        <div className="work">
+          <h3>Aug 2012 - Jun 2014</h3>
+          <h2>Head of Design at VIACOM</h2>
+          <p>
+            After the first two years as a designer I was promoted to leader of
+            the design team.  During that period we had two <a href="http://miaw.mtvla.com" target="_blank">MTV Awards</a> and two <a href="http://www.kidschoiceawards.com/" target="_blank">Kids Choice Awards</a> for all of Latin America. It was one of the most important
+            experiences in my career. I worked directly with all the departments
+            involved: marketing, production and sales.
+            <br/>
+            <br/>
+            I was responsible for:
+            <ul>
+              <li>Brand Identity System</li>
+              <li>Sales Presentations</li>
+              <li>Wireframing</li>
+              <li>Training</li>
+              <li>Recruiting</li>
+            </ul>
+          </p>
+        </div>
+        <div className="work">
+          <h3>Aug 2010 - Aug 2012</h3>
+          <h2>Web Designer at VIACOM</h2>
+          <p>I started working as a full-time designer for <a href="http://mtvla.com" target="_blank">MTV</a>, VH1, <a href="http://mundonick.com" target="_blank">Nickelodeon</a> and <a href="http://comedycentral.la" target="_blank">Comedy Central</a> — developing
+          all kinds of digital projects such as standalone websites,
+          marketing pages and social network assets for all the shows.</p>
+        </div>
+        <div className="work">
+          <h3>Jul 2009 - Aug 2010</h3>
+          <h2>Freelance</h2>
+        </div>
+        <div className="work">
+          <h3>Dec 2006 - Jul 2009</h3>
+          <h2>Designer at IDENTIDAD DG</h2>
+          <p>
+            For two and a half years I worked for the studio doing a variety of
+            projects, including brochures, photo edition and also some digital
+            presentations and websites. I am very proud of my work in 2008, when
+            I had the honor of designing the album <a href="https://www.discogs.com/John-Legend-Legend-Network-John-Legend-2008/release/11682137" target="_blank">The Legend Network</a> by <a href="https://www.johnlegend.com/" target="_blank">John Legend</a>.
+          </p>
+        </div>
+        <div className="work">
+          <h3>Oct 2006 - Jul 2008</h3>
+          <h2>Designer at TSYA</h2>
+        </div>
+      </div>
+      <div id="oss">
+        <h3>OSS Contributions</h3>
+        <div className="project">
+          <h2>Tipbox</h2>
+          <p><a href="https://tipbox.is" target="_blank">tipbox.is</a></p>
+          <p>Share a URL to collect anonymous feedback</p>
+        </div>
+        <div className="project">
+          <h2>Passport.js</h2>
+          <p><a href="http://passportjs.org" target="_blank">passportjs.org</a></p>
+          <p>Simple, unobtrusive authentication for Node.js</p>
+        </div>
+        <div className="project">
+          <h2>JWT.io</h2>
+          <p><a href="https://jwt.io" target="_blank">jwt.io</a></p>
+          <p>Allows you to decode, verify and generate JWT</p>
+        </div>
+        <div className="project">
+          <h2>MDX</h2>
+          <p><a href="https://mdxjs.com/" target="_blank">mdxjs.com</a></p>
+          <p>JSX in Markdown for ambitious projects</p>
+        </div>
+      </div>
+      <div id="press">
+        <h3>Press</h3>
+        <div className="article">
+          <h2>!important</h2>
+          <p><a href="https://xotv.me/channels/22-important/vod_videos/896-important-slash-slash-zeit-dot-co" target="_blank">https://xotv.me/channels/22-important/vod_videos/896-important-slash-slash-zeit-dot-co</a></p>
+        </div>
+        <div className="article">
+          <h2>1stWebDesigner</h2>
+          <p><a href="https://1stwebdesigner.com/material-design-guide/" target="_blank">1stwebdesigner.com/material-design-guide/</a></p>
+        </div>
+        <div className="article">
+          <h2>Beebom</h2>
+          <p><a href="https://beebom.com/best-material-design-apps-website/" target="_blank">beebom.com/best-material-design-apps-website/</a></p>
+        </div>
+        <div className="article">
+          <h2>DesignsHack</h2>
+          <p><a href="https://designshack.net/articles/ux-design/google-material-design-everything-you-need-to-know/" target="_blank">designshack.net/articles/ux-design/google-material-design-everything-you-need-to-know/</a></p>
+        </div>
+      </div>
+      <div id="education">
+        <h3>Education</h3>
+        <div className="degree">
+          <h2>Design</h2>
+          <p>Art, Design and Communication</p>
+        </div>
+        <div className="degree">
+          <h2>English</h2>
+          <p>First Certificate in English (Cambridge Essol Examinations)</p>
+        </div>
+      </div>
+    </div>
+
+    <style jsx>{`
+      a {
+        cursor: pointer;
+        color: #999;
+        line-height: 2.5em;
+        text-decoration: none;
+        transition: all .2s ease;
+      }
+
+      a:hover {
+        color: black;
+      }
+
+      .article {
+        margin-bottom: 50px;
+      }
+
+      .article p {
+        margin: 0;
+      }
+
+      .content {
+        width: 700px;
+      }
+
+      .degree {
+        margin-bottom: 50px;
+      }
+
+      .degree p {
+        margin: 0;
+      }
+
+      h2 {
+        font-size: 3em;
+        margin: 0;
+      }
+
+      h3 {
+        margin: 0;
+        text-transform: uppercase;
+      }
+
+      #education {
+        border-bottom: 1px solid #eaeaea;
+        margin-bottom: 100px;
+        padding-bottom: 50px;
+      }
+
+      #experience {
+        border-bottom: 1px solid #eaeaea;
+        margin-top: 150px;
+        margin-bottom: 100px;
+      }
+
+      li {
+        margin-bottom: 10px;
+      }
+
+      li:before {
+        content: '–';
+        display: inline-block;
+        color: #999;
+        position: absolute;
+        margin-left: -15px;
+      }
+
+      .logotype {
+        margin-bottom: 50px;
+        position: sticky;
+        top: 60px;
+      }
+
+      .navigation {
+        display: flex;
+        flex-direction: column;
+        position: sticky;
+        top: 150px;
+      }
+
+      #oss {
+        border-bottom: 1px solid #eaeaea;
+        margin-bottom: 100px;
+        padding-bottom: 50px;
+      }
+
+      p {
+        line-height: 2em;
+        color: #444;
+      }
+
+      p a {
+        border-bottom: 1px solid #FFF;
+        color: #0076FF;
+        line-height: 2em;
+      }
+
+      p a:hover {
+        border-bottom: 1px solid #0076FF;
+      }
+
+      #press {
+        border-bottom: 1px solid #eaeaea;
+        margin-bottom: 100px;
+        padding-bottom: 50px;
+      }
+
+      .project {
+        margin-bottom: 50px;
+      }
+
+      .project p {
+        margin: 0;
+      }
+
+      .sidebar {
+        padding: 50px;
+        width: 200px;
+      }
+
+      ul {
+        padding: 0;
+        list-style-type: none;
+        margin-left: 15px;
+      }
+
+      .work {
+        margin-bottom: 100px;
+      }
+
+      .wrapper {
+        margin: 0 auto;
+        max-width: 1000px;
+        display: flex;
+        justify-content: center;
+      }
+    `}</style>
+    <style global jsx>{`
+      body {
+        background-color: #fff;
+        color: #000;
+        font-family: "SF Pro Text","SF Pro Icons","Helvetica Neue","Helvetica","Arial",sans-serif;
+        font-size: 1em;
+        font-style: normal;
+        font-weight: 400;
+        margin: 0;
+        line-height: 1.47059;
+        letter-spacing: -.022em;
+      }
+    `}</style>
+  </div>
+)
